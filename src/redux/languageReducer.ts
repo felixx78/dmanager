70ec9d786b298066890a0bb145491b374def6adc
finn78 <petrenkov605@gmail.com>
2024-06-23 23:22:13 +0300

    refactor languageReducer

--- src/redux/languageReducer.ts
+++ src/redux/languageReducer.ts
@@ -4,7 +4,7 @@ const languageSlice = createSlice({
   name: "language",
   initialState: localStorage.getItem("language") || "en",
   reducers: {
-    set(state, action: PayloadAction<string>) {
+    set(_, action: PayloadAction<string>) {
       return action.payload;
     },
   },
