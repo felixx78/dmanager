dea71d6c73c1f28af2b362d2b3ee06027bc55eb8
finn78 <petrenkov605@gmail.com>
2024-06-23 21:52:02 +0300

    update RootLayout

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 
 function RootLayout() {
   return (
-    <Box>
+    <Box bgColor="gray.900" minH="100svh" color="gray.200" m={0} p={0}>
       <Outlet />
     </Box>
   );
