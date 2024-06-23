29f73646cefb5d90724c87701963cc200387197c
finn78 <petrenkov605@gmail.com>
2024-06-23 21:50:35 +0300

    add Inter font to Chakra theme

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,19 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import router from "./router.tsx";
 import "./index.css";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import "@fontsource-variable/inter";
+
+const theme = extendTheme({
+  fonts: {
+    heading: `'Inter Variable', sans-serif`,
+    body: `'Inter Variable', sans-serif`,
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <RouterProvider router={router} />
     </ChakraProvider>
   </React.StrictMode>
