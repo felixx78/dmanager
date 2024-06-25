d36abad642e780ea20239b37d0a8ee5368133a39
finn78 <petrenkov605@gmail.com>
2024-06-25 16:13:39 +0300

    add Home/Header

---
+++ src/components/Home/Header.tsx
@@ -0,0 +1,49 @@
+import { Button, Flex, Input } from "@chakra-ui/react";
+import { useForm } from "react-hook-form";
+import { useIntl } from "react-intl";
+
+type Props = {
+  amount?: number;
+  setAmount: React.Dispatch<React.SetStateAction<number | undefined>>;
+};
+
+type FormInputs = {
+  amount: number;
+};
+
+function HomeHeader({ amount, setAmount }: Props) {
+  const intl = useIntl();
+
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm<FormInputs>();
+
+  const handleOnSubmit = ({ amount }: FormInputs) => setAmount(amount);
+
+  return (
+    <Flex
+      mt={amount ? "0vh" : "30vh"}
+      as="form"
+      onSubmit={handleSubmit(handleOnSubmit)}
+      gap="20px"
+      transition="margin .3s ease"
+    >
+      <Input
+        {...register("amount", {
+          required: true,
+        })}
+        isInvalid={!!errors?.amount}
+        type="number"
+        errorBorderColor="red.500"
+        placeholder={intl.formatMessage({ id: "amount" })}
+        onWheel={(e: any) => e.target.blur()}
+      />
+      <Button type="submit" colorScheme="whiteAlpha" px="40px">
+        {intl.formatMessage({ id: "calculate" })}
+      </Button>
+    </Flex>
+  );
+}
+export default HomeHeader;
