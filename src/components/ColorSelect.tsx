5515c64fe79ae85d0e24442d757a724bd9cb7080
felixx78 <petrenkov605@gmail.com>
2024-07-27 15:47:19 +0300

    add ClickOutside to ColorSelect

--- src/components/ColorSelect.tsx
+++ src/components/ColorSelect.tsx
@@ -10,11 +10,7 @@ import {
 } from "@chakra-ui/react";
 import { useState } from "react";
 import capitalize from "../utils/capitalize";
-
-// type Props = {
-//   icon: string;
-//   setIcon: React.Dispatch<React.SetStateAction<string>>;
-// };
+import ClickOutside from "./ClickOutside";
 
 const colors = [
   "gray",
@@ -52,61 +48,65 @@ function ColorSelect({ color, setColor }: Props) {
         </Button>
       </PopoverTrigger>
 
-      <PopoverContent>
-        <Text
-          px="15px"
-          pt="5px"
-          mb="10px"
-          fontSize="18px"
-          fontWeight={500}
-          color="gray.700"
-        >
-          {capitalize(selectedBaseColor)}
-        </Text>
-        <SimpleGrid
-          px="10px"
-          columns={2}
-          spacing={2}
-          alignContent="center"
-          mb="20px"
-        >
-          {shades.map((i) => (
-            <Button
-              bgColor={
-                color === selectedBaseColor + "." + i ? "gray.300" : "gray.100"
-              }
-              onClick={() => handleChangeColor(selectedBaseColor, i)}
-              key={i}
-            >
+      <ClickOutside onClick={() => setIsOpen(false)} ignoreClick={!isOpen}>
+        <PopoverContent>
+          <Text
+            px="15px"
+            pt="5px"
+            mb="10px"
+            fontSize="18px"
+            fontWeight={500}
+            color="gray.700"
+          >
+            {capitalize(selectedBaseColor)}
+          </Text>
+          <SimpleGrid
+            px="10px"
+            columns={2}
+            spacing={2}
+            alignContent="center"
+            mb="20px"
+          >
+            {shades.map((i) => (
+              <Button
+                bgColor={
+                  color === selectedBaseColor + "." + i
+                    ? "gray.300"
+                    : "gray.100"
+                }
+                onClick={() => handleChangeColor(selectedBaseColor, i)}
+                key={i}
+              >
+                <Box
+                  bgColor={selectedBaseColor + "." + i}
+                  width="30px"
+                  height="30px"
+                />
+              </Button>
+            ))}
+          </SimpleGrid>
+          <Flex
+            px="20px"
+            pb="5px"
+            maxW="300px"
+            justifyContent="center"
+            gap="10px"
+            overflowX="auto"
+          >
+            {colors.map((i) => (
               <Box
-                bgColor={selectedBaseColor + "." + i}
-                width="30px"
-                height="30px"
-              />
-            </Button>
-          ))}
-        </SimpleGrid>
-        <Flex
-          px="20px"
-          pb="5px"
-          maxW="300px"
-          justifyContent="center"
-          gap="10px"
-          overflowX="auto"
-        >
-          {colors.map((i) => (
-            <Box
-              as="button"
-              display="block"
-              width="80px"
-              key={i}
-              onClick={() => setSelectedBaseColor(i)}
-            >
-              <Box mx="auto" bgColor={i + ".500"} w="15px" h="15px" />
-            </Box>
-          ))}
-        </Flex>
-      </PopoverContent>
+                as="button"
+                display="block"
+                width="80px"
+                key={i}
+                onClick={() => setSelectedBaseColor(i)}
+              >
+                <Box mx="auto" bgColor={i + ".500"} w="15px" h="15px" />
+              </Box>
+            ))}
+          </Flex>
+        </PopoverContent>
+      </ClickOutside>
     </Popover>
   );
 }
